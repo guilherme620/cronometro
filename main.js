431fe348060b738de186ad17a20e2decbdbc54cd
guilherme620 <guilherme.moreira.santos12@escola.pr.gov.br>
2026-06-08 14:37:24 +0000

    Salvar alterações e sincronizar

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
-
+const textos = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
 
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   };
 }
 
-botoes.forEach(botao => {
-  botao.addEventListener('click', function() {
-    // Remove classe 'ativo' de todos os botões
-    botoes.forEach(b => b.classList.remove('ativo'));
-    // Adiciona classe 'ativo' apenas ao botão clicado
-    this.classList.add('ativo');
-  });
-});
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
